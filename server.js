9a1d385302be80f1c78c9b1230234875486d6e3d
XfortunaX <buklin.sergei@yandex.ru>
2018-03-14 18:12:10 +0300

    deploy

--- server.js
+++ server.js
@@ -2,21 +2,7 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-(function initWebpack() {
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack/common.config');
-  const compiler = webpack(webpackConfig);
-
-  app.use(require('webpack-dev-middleware')(compiler, {
-    noInfo: true, publicPath: webpackConfig.output.publicPath,
-  }));
-
-  app.use(require('webpack-hot-middleware')(compiler, {
-    log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000,
-  }));
-
-  app.use(express.static(__dirname + '/'));
-})();
+app.use(express.static(__dirname + '/'));
 
 app.get(/.*/, function root(req, res) {
   res.sendFile(__dirname + '/index.html');
